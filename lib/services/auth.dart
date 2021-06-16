750a68ca072c0019683c34ff5b0031e4800e3116
Himanshugt <himan321gupta@gmail.com>
2021-06-17 01:40:49 +0530

    Stream added

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -9,8 +9,15 @@ class AuthService{
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //create user obj based on FirebaseUser
-  User _userFromFireBaseUSer(FirebaseUser user){
+  User _userFromFireBaseUser(FirebaseUser user){
     return user != null ? User(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+
+  Stream<User> get user{
+    return _auth.onAuthStateChanged
+        .map(_userFromFireBaseUser);
   }
 
   //sign in anon
@@ -18,7 +25,7 @@ class AuthService{
     try{
       AuthResult result=await _auth.signInAnonymously();
       FirebaseUser user=result.user;
-      return _userFromFireBaseUSer(user);
+      return _userFromFireBaseUser(user);
     }catch(e){
       print(e.toString());
       return null;
